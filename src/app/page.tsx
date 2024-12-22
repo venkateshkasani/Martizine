baf6c36f36cea50b76d3b17df973b45860861a87
Venkatesh <venkateshkasani14@gmail.com>
2024-12-22 20:27:56 +0530

    fix: middleware trigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-import Cookies from "js-cookie";
 
 export default async function Home() {
-  const cookie = Cookies.get('artk');
+
   return (
     <div className="">
-      Hello world
+      Hello world home
     </div>
   );
 }
-
-// export const getServerSideProps:GetServerSideProps = async (context) => {
-//     const cookies = parseCookies(context);
-//     const artk = cookies.artk;
-//     if(!artk){
-//       return {
-//         redirect:{
-//           destination:'/signin',
-//           permanent:false
-//         }
-//       }
-//     } else {
-//       return {
-//         props:{}
-//       }
-//     }
-// }
